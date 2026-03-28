cd3a814472fb75fb6fd2110ebf9b18d3e7b28241
Mohammed786561 <354253295@educ.dpcdsb.org>
2026-03-28 00:06:31 -0400

    Create simple.js for handling functionality related to simple.html

---
+++ js/simple.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function showSection(section) {
+    // logic to show the specified section
+}
+
+function selectLoadout(loadout) {
+    // logic to select a loadout
+}
+
+function selectMap(map) {
+    // logic to select a map
+}
+
+function startGame() {
+    // logic to start the game
+}
